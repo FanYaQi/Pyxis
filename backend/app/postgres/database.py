03c3c16cdf82069d85411119c86885c356003c78
Pikachi <omybadiafan@gmail.com>
2025-03-12 11:22:02 -0700

    (backend) configure database connection logger

--- backend/app/postgres/database.py
+++ backend/app/postgres/database.py
@@ -1,12 +1,16 @@
+import logging
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.configs.settings import settings
 
+# use uvicorn logger config to log the database connection to console
+logger = logging.getLogger("uvicorn.error")
 
 SQLALCHEMY_DATABASE_URL = str(settings.SQLALCHEMY_DATABASE_URI)
 
-print(f"Connecting to database: {SQLALCHEMY_DATABASE_URL}")
+logger.info("Connecting to database: %s", SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
